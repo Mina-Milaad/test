907a754388a47a7f06a4492668aca4c377687649
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-04 14:26:25 +0300

    script file

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
-export const dbConn = connect('mongodb://host.docker.internal:27017/test-Project')
+export const dbConn = connect('mongodb://mongo:27017/test-Project')
 .then(() => {
     console.log("database Connected Successfully");
 })
