e88fbe2a7e0cf6649542cae61a39765e8f471b06
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-07 15:07:36 +0300

    add cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { dbConn } from './database/dbConnection.js'
 import { globalError } from './src/middleware/globalError.js'
 import { AppError } from './src/utils/appError.js'
 import { bootstrap } from './src/modules/bootstrap.js'
+import cors from 'cors'
 
 const app = express()
 
 const port = 5000
+app.use(cors())
 app.use(express.json())
 bootstrap(app)
 app.use('*', (req, res, next) => {
